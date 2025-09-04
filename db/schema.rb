438dba451e58826a90c4cc000bb640ba8769c18d
bp-demostudent-1 <125912413+bp-demostudent-1@users.noreply.github.com>
2025-09-04 20:04:56 +0000

    Fix pg extensions for xata

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,8 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_04_191218) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  # Xata (production) has this enabled by default
+  enable_extension "pg_catalog.plpgsql" unless ENV["DATABASE_URL"]&.include?("xata.sh")
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
